f095ae1694298537bda1e185442da892b0fa4aa3
aragami3070 <smirnov17612@gmail.com>
2025-02-04 20:36:51 +0400

    Add: Swagger

--- Controllers/Program.cs
+++ Controllers/Program.cs
@@ -3,9 +3,17 @@ var builder = WebApplication.CreateBuilder(args);
 // Add services to the container.
 builder.Services.AddControllersWithViews();
 
+//Add Swagger
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
 
 var app = builder.Build();
 
+//Add Swagger
+if (app.Environment.IsDevelopment()){
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
 
 app.UseRouting();
 
