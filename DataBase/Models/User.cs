11cd24e9f60138a1fb474483e26f9cf3f162db5c
aragami3070 <smirnov17612@gmail.com>
2025-02-05 16:05:22 +0400

    Add: User model

---
+++ DataBase/Models/User.cs
@@ -0,0 +1,9 @@
+namespace DataBase;
+
+// Class User
+public class User{
+    public string FirstName { get; set; }
+    public string LastName { get; set; }
+    public string Email { get; set; }
+    public string Password { get; set; }
+}
