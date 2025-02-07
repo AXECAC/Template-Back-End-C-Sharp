0300836cfeec63eb4c08e3f3e75f7cbb6189edd9
aragami3070 <smirnov17612@gmail.com>
2025-02-07 22:49:50 +0400

    Fix: User model field

--- DataBase/Models/User.cs
+++ DataBase/Models/User.cs
@@ -2,7 +2,7 @@ namespace DataBase;
 
 // Class User
 public class User{
-    public int? Id { get; set; }
+    public int Id { get; set; }
     public string FirstName { get; set; }
     public string SecondName { get; set; }
     public string Email { get; set; }
