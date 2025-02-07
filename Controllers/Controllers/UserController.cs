c0b52918764a12ca71b254d8486921c6669d315b
aragami3070 <smirnov17612@gmail.com>
2025-02-07 16:43:03 +0400

    Add: GetUsers() endpoint

--- Controllers/Controllers/UserController.cs
+++ Controllers/Controllers/UserController.cs
@@ -9,19 +9,33 @@ namespace Controllers.UserController
     public class UserController
     {
         private readonly IUserServices _UserServices; 
-
+        
         public UserController(IUserServices userServices)
         {
             _UserServices = userServices;
         }
+        
         // Some Get method
         [HttpGet]
-        public IActionResult SomeGet()
+        public async Task<IResult> GetUsers()
         {
-            // Return response 200
-            return new OkResult();
+            var response = await _UserServices.GetUsers();
+                   
+            // Some Users find
+            if (response.StatusCode == 200)
+            {
+                return Results.Ok(response.Data.ToList());
+            }
+            // 0 Users find
+            if (response.StatusCode == 204)
+            {
+                // Return response 200
+                return Results.Ok();
+            }
+                   
+            return Results.StatusCode(statusCode: response.StatusCode);
         }
-
+        
         // Some Post method
         [HttpPost]
         public IActionResult SomePost(int some)
