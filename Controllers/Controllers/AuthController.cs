84a2e645f92f7c1cd13b78d47a1ad67c0b7dea76
aragami3070 <smirnov17612@gmail.com>
2025-02-21 18:01:02 +0400

    Update: Register endpoint

--- Controllers/Controllers/AuthController.cs
+++ Controllers/Controllers/AuthController.cs
@@ -2,23 +2,48 @@
 /* using Microsoft.AspNetCore.Identity; */
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
+using Services;
 using DataBase;
 
 namespace Controllers.AuthController
 {
-    [Authorize]
+    // [Authorize]
     [Route("api/[controller]/[action]")]
     [ApiController]
     // StartController class controller
     public class AuthController : Controller
     {
+        private readonly IAuthServices _AuthServices;
+        private readonly string? _secretKey;
+
+        public AuthController(IAuthServices authServices, IConfiguration configuration)
+        {
+            _AuthServices = authServices;
+            _secretKey = configuration.GetValue<string>("ApiSettings:Secret");
+        }
+
         // Some Post method
         [HttpPost]
         [AllowAnonymous]
-        [ValidateAntiForgeryToken]
-        public async Task<IActionResult> Register(User test)
+        // [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Register(User user)
         {
-            return  Ok();
+            if (_secretKey != "")
+            {
+                var response = await _AuthServices.TryRegister(user, _secretKey);
+                if (response.StatusCode == DataBase.StatusCodes.Created)
+                {
+                    Console.WriteLine(response.Data);
+                    return Ok(new {response.Data});
+                }
+                else
+                {
+                    return Conflict();
+                }
+            }
+            // Return StatusCode 500
+            return StatusCode(statusCode: 500);
+            
         }
     }
 }
