742f0a460183ac4d41de63d9285cd595b76a16dc
aragami3070 <smirnov17612@gmail.com>
2025-02-21 18:00:17 +0400

    Add: jwt support to Program.cs

--- Controllers/Program.cs
+++ Controllers/Program.cs
@@ -1,4 +1,7 @@
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.IdentityModel.Tokens;
 using Microsoft.EntityFrameworkCore;
+using System.Text;
 using Services;
 using Context;
 
@@ -10,14 +13,35 @@ builder.Services.AddControllersWithViews();
 //Add Swagger
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
-
+// Add services to the container.
+builder.Services.AddAuthentication(options =>
+        {
+        options.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+        options.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+        })
+.AddJwtBearer(options =>
+            {
+            options.TokenValidationParameters = new TokenValidationParameters
+            {
+                ValidateIssuer = true,
+                ValidateAudience = true,
+                ValidateLifetime = true,
+            ValidateIssuerSigningKey = true, 
+            ValidIssuer = "yourdomain.com",
+            ValidAudience = "yourdomain.com",
+            IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(builder.Configuration["ApiSettings:Secret"]))
+            };
+ });
 // Add my Services
 builder.Services.AddScoped<IUserRepository, UserRepository>();
 builder.Services.AddScoped<IUserServices, UserServices>();
+builder.Services.AddScoped<ITokenServices, TokenServices>();
 builder.Services.AddScoped<IHashingServices, HashingServices>();
+builder.Services.AddScoped<IAuthServices, AuthServices>();
 
 // Read connection string to pgsql db
 var connectionString = builder.Configuration.GetConnectionString("Postgres");
+
 
 // Connect to db
 builder.Services.AddDbContext<TemplateDbContext>(options => 
