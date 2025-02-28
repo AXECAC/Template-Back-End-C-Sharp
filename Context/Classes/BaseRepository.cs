b6782723814bbdc6d2bd8492ce5b776ff9d8c09f
aragami3070 <smirnov17612@gmail.com>
2025-02-28 19:23:48 +0400

    Add: BaseRepository implementation

---
+++ Context/Classes/BaseRepository.cs
@@ -0,0 +1,48 @@
+using Microsoft.EntityFrameworkCore;
+using DataBase;
+
+namespace Context;
+
+// Class UserRepository
+public class BaseRepository<T> : IBaseRepository<T> where T : class
+{
+    private readonly TemplateDbContext Db;
+
+    public BaseRepository(TemplateDbContext db)
+    {
+        Db = db;
+    }
+    
+
+    // Create model in db
+    public async Task<bool> Create(T model)
+    {
+        await Db.Set<T>().AddAsync(model);
+        await Db.SaveChangesAsync();
+        return true;
+    }
+
+    // GeUser models from db
+    public async Task<List<T>> Select()
+    {
+        return await Db.Set<T>().ToListAsync();
+    }
+
+    // Delete models from db
+    public async Task<bool> Delete(T model)
+    {
+        Db.Set<T>().Remove(model);
+        await Db.SaveChangesAsync();
+
+        return true;
+    }
+
+    // Update model in db
+    public async Task<T> Update(T model)
+    {
+        Db.Set<T>().Update(model);
+        await Db.SaveChangesAsync();
+
+        return model;
+    }
+}
