660a5149326832e9de9e64c9eb9cf6d8ae6faa25
aragami3070 <smirnov17612@gmail.com>
2025-02-28 19:23:14 +0400

    Update: Repositories interfaces

--- Context/Interfaces/IUserRepository.cs
+++ Context/Interfaces/IUserRepository.cs
@@ -5,6 +5,9 @@ namespace Context;
 // Interface IUserRepository
 public interface IUserRepository : IBaseRepository<User>
 {
+    // Get model from db by id
+    Task<User>? Get(int id);
+
     // Get user by email
     Task<User>? GetByEmail(string email);
 }
