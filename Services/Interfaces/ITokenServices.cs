30b96b40fd1b01bcf49f589f867e687f33dc7565
aragami3070 <smirnov17612@gmail.com>
2025-05-11 15:45:06 +0400

    Fix: response type for RefreshToken in TokenServices

--- Services/Interfaces/ITokenServices.cs
+++ Services/Interfaces/ITokenServices.cs
@@ -6,5 +6,5 @@ public interface ITokenServices
 {
     public Task<Tokens> GenerateJWTToken(User user, string secretKey);
 
-    public Task<BaseResponse<Tokens>> RefreshToken(string oldRefreshToken, string secretKey);
+    public Task<IBaseResponse<Tokens>> RefreshToken(string oldRefreshToken, string secretKey);
 }
